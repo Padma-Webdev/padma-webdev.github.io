e0c02e3325e72f4b04538f30da75d27f01f93e34
Padma <padmarajugangurmallesha@gmail..com>
2026-01-13 18:48:49 +0000

    leaving certificate

--- src/components/website/courses/irelandCourses/JuniorCert/juniorCert.tsx
+++ src/components/website/courses/irelandCourses/JuniorCert/juniorCert.tsx
@@ -1,17 +1,42 @@
 import React from "react";
 import BackButton from "../../../common/back-button";
-import { View, Text, Dimensions, ScrollView } from "react-native";
+import { View, Text, Dimensions, ScrollView, TouchableOpacity, Linking, ImageBackground, } from "react-native";
 import { hpatStyles } from "../HPAT/hpatStyles";
 import { groupStyles } from "../HPAT_Tutoring/groupPrepStyles";
 import { JuniorStyles } from "./juinosStyles";
+import { SafeAreaView,SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function JuniorCertificateContainer() {
   const screenWidth = Dimensions.get("window").width;
   const isSmallScreen = screenWidth < 740;
+    const image = {
+    uri:"https://images.pexels.com/photos/6549862/pexels-photo-6549862.jpeg?_gl=1*ubd2sz*_ga*MjEyMzczNTM1Ny4xNzY2MDgyMzEw*_ga_8JE65Q40S6*czE3NjgzMjgxMjIkbzYkZzEkdDE3NjgzMjk0NTIkajQ0JGwwJGgw"
+  };
 
   const Header = () => {
     return (
-      <View>
+      <View
+        style={{
+          alignSelf: "center",
+          width: "100%",
+          marginBottom: 10,
+          borderRadius: 5,
+          borderWidth: 3,
+          padding: 2,
+          borderColor: "#23CFBB",
+        }}
+      >
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: "#080e19",
+            opacity: 0.5,
+          }}
+        />
         <Text style={JuniorStyles.mainText}>Irish Junior Certificate</Text>
         <Text style={hpatStyles.hpatDesc}>
           Your path to studying medicine begins with the Irish Junior
@@ -42,27 +67,77 @@ export default function JuniorCertificateContainer() {
         <Text style={JuniorStyles.hpatDesc2}>
           Every UK medical school has different IJC grade requirements. MedTutor
           offers personalised advice on which grades you need for your target
-          universities, ensuring you have a clear roadmap to success. 
+          universities, ensuring you have a clear roadmap to success.
         </Text>
         <Text style={JuniorStyles.hpatDesc}>
           At MedTutor, we provide tailored advice based on your academic profile
           and university goals, helping you map out a clear, confident path to
           medical school success.
         </Text>
+        <TouchableOpacity style={
+                    isSmallScreen
+                      ? {
+                          backgroundColor: "#2563EB",
+                          borderRadius: 10,
+                          padding: 5,
+                          width: "95%",
+                          alignSelf: "center",
+                          marginBottom:15,
+                        }
+                      : {
+                          backgroundColor: "#2563EB",
+                          borderRadius: 5,
+                          padding: 5,
+                          width: 900,
+                          alignSelf: "center",
+                          alignContent: "center",
+                          marginBottom:10,
+                        }
+                  }
+                  onPress={() => {
+                    Linking.openURL(
+                      "mailto: contactus@medtutor.ie?subject=Junior Certificate Tutoring"
+                    );
+                  }}>
         <Text style={groupStyles.Bookings}>
           Email us at contactus@medtutor.ie to book your personalised
           consultation.
         </Text>
+        </TouchableOpacity>
       </View>
     );
   };
 
   return (
     <ScrollView style={hpatStyles.mainScroll}>
-      <View style={hpatStyles.backButton}>
-        <BackButton />
-      </View>
+      <SafeAreaProvider>
+              <SafeAreaView>
+                <ImageBackground
+                  source={image}
+                  style={{
+                    flex: 1,
+                  }}
+                >
+                  <View
+                    style={{
+                      borderRadius: 15,
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      backgroundColor: "#080e19",
+                      opacity: 0.3,
+                    }}
+                  />
+                  <View style={hpatStyles.backButton}>
+                    <BackButton />
+                  </View>
+
       <View style={JuniorStyles.topHeader}>{Header()}</View>
+                </ImageBackground>
+              </SafeAreaView>
+            </SafeAreaProvider>
     </ScrollView>
   );
 }
--- src/components/website/courses/irelandCourses/JuniorCert/leavingCert.tsx
+++ src/components/website/courses/irelandCourses/JuniorCert/leavingCert.tsx
@@ -1,17 +1,40 @@
 import React from "react";
 import BackButton from "../../../common/back-button";
-import { View, Text, Dimensions, ScrollView } from "react-native";
+import { View, Text, Dimensions, ScrollView, Linking, TouchableOpacity, ImageBackground } from "react-native";
 import { hpatStyles } from "../HPAT/hpatStyles";
 import { groupStyles } from "../HPAT_Tutoring/groupPrepStyles";
 import { JuniorStyles } from "./juinosStyles";
+import { SafeAreaView,SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function LeavingCertContainer() {
   const screenWidth = Dimensions.get("window").width;
   const isSmallScreen = screenWidth < 740;
+      const image = {
+    uri:"https://images.pexels.com/photos/8197511/pexels-photo-8197511.jpeg?_gl=1*3oei9j*_ga*MjEyMzczNTM1Ny4xNzY2MDgyMzEw*_ga_8JE65Q40S6*czE3NjgzMjgxMjIkbzYkZzEkdDE3NjgzMzAwMzAkajU2JGwwJGgw",
+  };
 
   const Header = () => {
     return (
-      <View>
+      <View style={{
+          alignSelf: "center",
+          width: "100%",
+          marginBottom: 10,
+          borderRadius: 5,
+          borderWidth: 3,
+          padding: 2,
+          borderColor: "#23CFBB",
+        }}>
+          <View
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      backgroundColor: "#080e19",
+                      opacity: 0.5,
+                    }}
+                  />
         <Text style={JuniorStyles.mainText}>Leaving Certificate</Text>
         <Text style={hpatStyles.hpatDesc}>
           Medicine and Dentistry are among the most academically demanding
@@ -56,20 +79,70 @@ export default function LeavingCertContainer() {
           need for your target universities, ensuring you have a clear roadmap
           to success.
         </Text>
-        <Text style={groupStyles.Bookings}>
-          Email us at contactus@medtutor.ie to book your personalised
-          consultation.
-        </Text>
+       <TouchableOpacity style={
+                           isSmallScreen
+                             ? {
+                                 backgroundColor: "#2563EB",
+                                 borderRadius: 10,
+                                 padding: 5,
+                                 width: "95%",
+                                 alignSelf: "center",
+                                 marginBottom:15,
+                               }
+                             : {
+                                 backgroundColor: "#2563EB",
+                                 borderRadius: 5,
+                                 padding: 5,
+                                 width: 900,
+                                 alignSelf: "center",
+                                 alignContent: "center",
+                                 marginBottom:10,
+                               }
+                         }
+                         onPress={() => {
+                           Linking.openURL(
+                             "mailto: contactus@medtutor.ie?subject=Leaving Certificate Tutoring"
+                           );
+                         }}>
+               <Text style={groupStyles.Bookings}>
+                 Email us at contactus@medtutor.ie to book your personalised
+                 consultation.
+               </Text>
+               </TouchableOpacity>
       </View>
     );
   };
 
   return (
     <ScrollView style={hpatStyles.mainScroll}>
-      <View style={hpatStyles.backButton}>
-        <BackButton />
-      </View>
+      <SafeAreaProvider>
+              <SafeAreaView>
+                <ImageBackground
+                  source={image}
+                  style={{
+                    flex: 1,
+                  }}
+                >
+                  <View
+                    style={{
+                      borderRadius: 15,
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      backgroundColor: "#080e19",
+                      opacity: 0.3,
+                    }}
+                  />
+                  <View style={hpatStyles.backButton}>
+                    <BackButton />
+                  </View>
+
       <View style={JuniorStyles.topHeader}>{Header()}</View>
+                </ImageBackground>
+              </SafeAreaView>
+            </SafeAreaProvider>
     </ScrollView>
   );
 }
