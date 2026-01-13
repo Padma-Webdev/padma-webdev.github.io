c927719d46d1a1cb37232d4284bfb53cf05131e4
Padma <padmarajugangurmallesha@gmail..com>
2026-01-13 19:54:02 +0000

    new

--- src/components/website/Tutors/index.tsx
+++ src/components/website/Tutors/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text, Dimensions, Image } from "react-native";
 import { courseStyles } from "../courses/styles";
 import { useNavigation } from "@react-navigation/native";
-import AbbyImage from '../../assets/images/ABBY.jpeg'
+import AbbyImage from '../../images/ABBY.jpeg'
 
 export default function TutorsContainer() {
   const screenWidth = Dimensions.get("window").width;
